719316b81a64110d3d60be9f2dea9ad4439a181e
Nathaniel Forsyth <ironsideforsyth@gmail.com>
2022-07-26 15:26:03 +0100

    first test passed

---
+++ models/specs/room_specs.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const Room = require('../room')
+
+describe('Room', function () {
+    let room;
+
+    beforeEach(function () {
+        room = new Room(10);
+    });
+
+    it('should have an area', function () {
+        const actual = room.area
+        assert.strictEqual(actual, 10)
+    })
+})
